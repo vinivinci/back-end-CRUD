ed989df6449540a55d552986183e77f2a5f78f6b
vinivinci <viniciusataides@gmail.com>
2021-02-28 15:53:53 -0300

    Heroku config

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/noderest", {
+mongoose.connect("mongodb+srv://admin:admin@cluster0.3736q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
